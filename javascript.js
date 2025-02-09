a193d83d99fe637176fd3b752a4bee620ec5399b
HGK.2010 <hg939295@gmail.com>
2025-02-09 17:44:52 +0330

    BOM finished

---
+++ javascript.js
@@ -0,0 +1,81 @@
+// Hi Lets go for new Java sript in broesers:
+
+// console.log(window.alert("In the name of God"));
+
+// از این به بعد دیگه لاگ نمیگیرم
+
+// let a = open("https://www.youtube.com/");
+
+// اینجا نمیگیره
+
+// let confirmAll = confirm("Ok or cancel??!!");
+
+// console.log(confirmAll);
+
+// print;
+// پرینت میگیره
+
+// const inPut = prompt("put yowr password MAN");
+// console.log(inPut);
+
+// stop();
+// Dont load
+
+// const timing = setTimeout(() => {
+//     console.log("Alooooo")
+// },10000);
+
+// console.log(timing);
+// clearTimeout(timing)
+
+// const timing2 = setInterval(() => {
+//     console.log("Alooooo")
+// }, 500);
+
+// console.log(timing2);
+
+
+// console.log(innerWidth);
+// matchMedia("(with : 700px)").machingIt;
+// console.log(matchMedia.machingIt);
+
+// console.log(location.protocol);
+// console.log(location.hostname);
+// console.log(location.port);
+// console.log(location.pathname);
+// // location.origin is logging port and host
+
+// location.assign("https://chatgpt.com/");
+
+// با هیستوری آشنا شو
+
+// history.back();
+// history.forward();
+// console.log(history.length);
+// history.go(-1);
+
+
+
+// کوکی هوووو
+
+// console.log(screen.width);
+// console.log(screen.height);
+// console.log(screen.availHeight);
+// console.log(screen.availWidth);
+
+// console.log(new Date("2020/1/1").toUTCString());
+// document.cookie = "name = Ali; expires = Tue, 31 Dec 2019 20:30:00 GMT";
+
+
+// // LOCAL storage
+
+// localStorage.setItem("manage" , "Its managing");
+// localStorage.setItem("name" , "ALi");
+
+// localStorage.removeItem("manage");
+// localStorage.clear()
+
+
+// sessionStorage.clear();
+// // هست فقط اینکه در همون تب فقط در دسترس هست و بعد پاک میشه Loavlstorage در واقع همون sessionStorage 
+// // حواست باشه پراپرتی و متود ها هم همونه
